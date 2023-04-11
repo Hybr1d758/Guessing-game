179d2aac36ecab6f6d33e1f5c7afc3fd38b27b00
Hybr1d758 <127874717+Hybr1d758@users.noreply.github.com>
2023-04-11 00:02:32 -0700

    Initial commit

---
+++ guess.py
@@ -0,0 +1,18 @@
+#write a program, that generates a random number
+import random
+number = random.randint(1, 100)
+
+guess = 0
+guess_count = 0
+
+while guess != number:
+    guess = int(input('Enter any number:'))
+    if guess < number:
+        print("Wrong Guess")
+    elif guess > number:
+        print("Wrong Guess")
+    else:
+        print("You got it right")
+
+
+
