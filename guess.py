fdc3f9d223b06b04f7fc84ecec6f7d53e8e69722
Hybr1d758 <127874717+Hybr1d758@users.noreply.github.com>
2023-04-19 00:17:20 -0700

    am confused
    i think i got everything right but there's a bug

--- guess.py
+++ guess.py
@@ -5,18 +5,27 @@ number = random.randint(1, 100)
 print("What is your name?")
 name = input()
 
-guess_count = 7
+guess_count = 4
 guess = 0
-print(name + " this is a guess game be sure to enter number that comes to mind.")
-while guess != number:
+print(name + ", this is a guess game be sure to enter number that comes to mind.")
+while guess < 4:
     guess = int(input('Enter any number:'))
-    if guess < number:
-        print("Wrong Guess")
-    elif guess > number:
-        print("Wrong Guess")
+    if guess % 2:
+        print("The answer is an odd number")
     else:
-        print("You got it right")
-
+        print("The answer is an even number")
+        break
+    if guess > number:
+        print("The answer is lower than the number")
+    else:
+        print("The answer is greater than the number")        
+        break
+    if guess == number:
+        print("Well done, you got it right")
+    else:
+        print("Wrong answer! you have run put attempts")
+    
+     
 
 
 
